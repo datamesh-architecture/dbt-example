3c914234f2d108ce5540b403bffb797d45e176de
jochen <jochen.christ@innoq.com>
2022-09-26 15:20:20 +0200

    WIP

--- models/email_domains_grouped.sql
+++ models/email_domains_grouped.sql
@@ -1,5 +1,12 @@
+with subscription as (
+    select * from {{ source('operational_system', 'subscription') }}
+),
+
+email_provider as (
+    select * from {{ ref('email_provider')}}
+)
+
 select name, split_part(email, '@', 2) as email_domain, count(*) as email_domain_count
-from {{ source('operational_system', 'subscription') }} u left join {{ ref('email_provider')}} e
-on lower (split_part(u.email, '@', 2)) = e.domain
+from subscription u left join email_provider e on lower (split_part(u.email, '@', 2)) = e.domain
 group by rollup (name, email_domain)
 order by name, email_domain
