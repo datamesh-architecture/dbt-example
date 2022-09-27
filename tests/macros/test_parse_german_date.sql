6cdc00030a0ac99e4fda758ddd6c0d95795fc3bc
Simon Harrer <simon.harrer@gmail.com>
2022-09-27 10:07:49 +0200

    Update

--- tests/macros/test_parse_german_date.sql
+++ tests/macros/test_parse_german_date.sql
@@ -1,7 +1,4 @@
-with testdata as (select '11.01.2022' as inputdata, make_date(2022, 1, 11) as expectedoutput),
-     tests as (select {{ parse_german_date(inputdata) }} as actual, expectedoutput as expected
-from testdata)
+with test as (select {{ parse_german_date('\'11.01.2022\'') }} as actual, make_date(2022, 1, 11) as expected)
 select *
-from tests
-where tests.actual != tests.expected
-
+from test
+where test.actual != test.expected
