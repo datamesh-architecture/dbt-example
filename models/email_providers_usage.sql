cea1541c438323e91e29be47bf999e453542add5
Simon Harrer <simon.harrer@gmail.com>
2023-03-19 13:16:44 +0100

    Update

---
+++ models/email_providers_usage.sql
@@ -0,0 +1,2 @@
+select 'T-Online' as email_provider, 10 as email_address_count
+
--- models/number_of_active_subscriptions.sql
+++
@@ -1,7 +0,0 @@
-with subscriptions as (
-    select * from {{ ref('subscriptions') }}
-)
-
-select count(*)
-from subscriptions s
-where is_active = true
