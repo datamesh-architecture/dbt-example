669efd11234c3f939419ee071947bdc6512c5ddb
Simon Harrer <simon.harrer@gmail.com>
2023-03-21 10:30:47 +0100

    Update

--- backup/models/email_providers_usage.sql
+++ backup/models/email_providers_usage.sql
@@ -6,7 +6,7 @@ emails_with_domain as (
     from emails
 ),
 emails_grouped_by_email_provider as (
-    select email_providers.name as email_provider, count (*) as email_address_count
+    select email_providers.name as email_provider, count (*)::integer as email_address_count
     from email_providers, emails_with_domain
     where emails_with_domain.email_domain = email_providers.domain
     group by email_providers.name
